a60bc7d65ce045ba6ebae499c6cbe08752b8f427
unknown <daviddjustt@gmail.com>
2025-10-29 11:53:36 -0300

    Prisma colocdado. Primeira migration feita

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
-
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,       
-      envFilePath: '.env',   
-    }),
-    UsersModule,
-  ],
+  imports: [UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
